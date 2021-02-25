2b2fb4f5e3553e03df8d917939629bc57b80c026
Lingeswaran <mail2lingesh96@gmail.com>
2021-02-25 19:50:58 +0530

    heroku updation

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('auth\login');
+    return view('\resources\views\auth\login');
 });
 
 Auth::routes();
-
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
